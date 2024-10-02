8374593697f23fd561d413821895f58b4c631813
CarsonH19 <questline.carson@gmail.com>
2024-10-01 22:51:39 -0400

    fix: bugs

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,10 +10,10 @@ const RecentProjects = () => {
   return (
     <div className="py-20" id="projects">
       <h1 className="heading">
-        A Small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        A Small Selection of{" "}
+        <span className="text-purple">Recent Projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-6">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
